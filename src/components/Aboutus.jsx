ede95454214bfc103819a7ea10e8feb1b51ccf46
PkTheCoda <pranavkonjeti@gmail.com>
2024-01-19 18:00:28 -0500

    pushing new ui component + update

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Aboutus = () => {
+  return (
+    <div className='flex gap-10 py-10 bg-stone-50'>
+        <div className='w-1/2 py-10 flex flex-col items-end gap-y-5'>
+            <div className='w-5/6  flex flex-col gap-y-4'>
+                    <div className='flex flex-col gap-y-1'>
+                        <h3 className='text-base text-slate-700'>Making Change, one case at a time.</h3>
+                        <h1 className='text-5xl font-medium text-slate-700'>
+                            About Our Firm
+                        </h1>
+                    </div>
+                    <div className='pl-4 text-base border-l-2 border-black-500/40 text-slate-500 leading-relaxed tracking-wide'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sunt quasi perspiciatis odit perferendis deleniti vel 
+                        laborum voluptatem illum, numquam nam totam, inventore maxime, rerum quas ut fugit cumque exercitationem.
+                        <br />
+                        <br />
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut alias, placeat consequatur harum, cumque magnam delectus 
+                        minus eum laudantium recusandae aliquam voluptatem amet ipsam dolores tenetur ullam omnis distinctio odit!
+                        <br />
+                        <br />
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut alias, placeat consequatur harum, cumque magnam delectus 
+                        minus eum laudantium recusandae aliquam voluptatem amet ipsam dolores tenetur ullam omnis distinctio odit!
+                    </div>
+            </div>
+            <div className='pb-4 w-5/6'>
+            <a href="#_" class="inline-flex items-center w-full px-5 py-2 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-blue-500 border border-transparent border-solid transitionform cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-blue-700 hover:border-blue-700 hover:text-white focus-within:bg-blue-700 focus-within:border-blue-700">
+                Our Services
+                <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+            </a>
+            </div>
+        </div>
+            <div className="relative w-1/2 flex justify-center items-center">
+                {/* Image */}
+                <img
+                    src="https://static.vecteezy.com/system/resources/thumbnails/017/034/338/original/business-and-lawyers-discussing-contract-papers-with-brass-scale-on-desk-in-office-law-legal-services-advice-justice-and-law-concept-picture-with-film-grain-effect-free-video.jpg"
+                    className="rounded-xl z-30 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] w-10/12"
+                    alt="Legal discussion"
+                />
+            </div>
+    </div>
+  )
+}
+
+export default Aboutus
--- src/components/HeroMain.jsx
+++ src/components/HeroMain.jsx
@@ -16,27 +16,36 @@ const HeroMain = () => {
             <div class="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-20"
                 style={containerStyle}>
             </div>
-            <div className='h-max py-5 w-[60%] text-white flex flex-col gap-y-4 z-10'>
-                <div className='text-7xl font-semibold flex flex-col'>
-                        <div className='font-medium text-3xl text-blue-400'>
-                            Experience. Results. Communication.
+                <div className='h-max py-5 w-full  text-white flex gap-y-4 z-10'>
+                    <div className='w-7/12  flex flex-col gap-y-4 z-10'>
+                        <div className='text-7xl font-semibold flex flex-col'>
+                            <div className='font-medium text-3xl text-blue-400'>
+                                Experience. Results. Communication.
+                            </div>
+                            <div>
+                                Birmingham Criminal
+                                <span className="block">Defense Lawyers</span>
+                            </div>
                         </div>
-                        <div>
-                            Birmingham Criminal
-                            <span className="block">Defense Lawyers</span>
+                        <div className="text-xl w-2/3">
+                            Pooling Expertise, Ideas, and Insights
+                            Across England for Your Benefit
+                        </div>
+                        <div className='w-10/12 font-semibold h-max flex flex-col lg:flex-row gap-4'>
+                            <button className='w-5/12 px-5 py-2 text-center border-2 border-white transitionform duration-700 hover:border-transparent hover:bg-blue-500'>
+                                READ OUR REVIEWS
+                            </button>
+                            <button className='w-7/12 px-5 py-2 text-center border-2 border-transparent transitionform hover:bg-blue-800 duration-700 bg-blue-500'>
+                                FREE ONLINE CONSULTATION
+                            </button>
                         </div>
                     </div>
-                    <div className="text-xl w-2/3">
-                        Pooling Expertise, Ideas, and Insights
-                        Across England for Your Benefit
-                    </div>
-                    <div className='w-10/12 font-semibold h-max flex flex-col lg:flex-row gap-4'>
-                        <button className='w-5/12 px-5 py-2 text-center border-2 border-white transitionform duration-700 hover:border-transparent hover:bg-blue-500'>
-                            READ OUR REVIEWS
-                        </button>
-                        <button className='w-7/12 px-5 py-2 text-center border-2 border-transparent transitionform hover:bg-blue-800 duration-700 bg-blue-500'>
-                            FREE ONLINE CONSULTATION
-                        </button>
+                    <div className='w-5/12 flex items-center justify-center'>
+                        <img 
+                            src="https://media.istockphoto.com/id/1393509458/video/selective-focus-on-gavel-hammer.jpg?s=640x640&k=20&c=NjiP9h8bhyU8dc6V4oYVUh6QLFfHctIdRhPSEgh_UcU=" 
+                            alt=""
+                            className='rounded-2xl rotate-3 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]' 
+                        />
                     </div>
                 </div>
         </div>
