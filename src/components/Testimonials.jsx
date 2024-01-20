b1eab2d0acb5128a902348308e7fad4b16e611cd
PkTheCoda <pranavkonjeti@gmail.com>
2024-01-20 17:47:39 -0500

    removed box

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <div className="w-full flex flex-col gap-6 items-center justify-center py-[8rem] bg-stone-50">
-        <h1 className="w-5/6 bg-red-100 text-6xl font-medium tracking-tight">What our <span className="text-blue-600">Users say:</span></h1>
+        <h1 className="w-5/6 text-6xl font-medium tracking-tight">What our <span className="text-blue-600">Users say:</span></h1>
         <div className="w-5/6 flex flex-wrap justify-between gap-5">
             <figure class="w-full sm:w-[45%] lg:w-[30%] relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10 mb-8">
                 <svg aria-hidden="true" width="105" height="78" class="absolute top-6 left-6 fill-slate-200">
